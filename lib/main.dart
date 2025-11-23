a66c1d842f2ecf4e6dd3974a6c80d386e856207b
13all13ase <q13all13ase@outlook.jp>
2025-11-23 11:51:41 +0900

    feature: add Sacffold.body
    add SafeArea and SubscriptionList
    wrap with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sabakan/ui/subscription_list.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: SafeArea(child: SubscriptionList())),
     );
   }
 }
